a767ef6379db20af7ae6326beb3ed1fc644b0c8c
Hossam Tarek <hossamtarek250@gmail.com>
2018-07-22 17:51:10 +0200

    Add fetch data utilities

---
+++ app/src/main/java/com/example/hossam/news/QueryUtils.java
@@ -0,0 +1,124 @@
+package com.example.hossam.news;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
+public final class QueryUtils {
+    private static final String TAG = "QueryUtils";
+
+    private QueryUtils() {}
+
+    public static ArrayList<Item> fetchNewsData(String stringUrl) {
+        URL url = createUrl(stringUrl);
+
+        String jsonResponse = null;
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e(TAG, "Error making http request.", e);
+        }
+        return extractNewsFromJson(jsonResponse);
+    }
+
+    public static URL createUrl(String stringUrl) {
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "Error with creating URL", e);
+        }
+        return url;
+    }
+
+    private static String readFromStream(InputStream inputStream) throws IOException{
+        StringBuilder stringBuilder = new StringBuilder();
+
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream,
+                    Charset.forName("UTF-8"));
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                stringBuilder.append(line);
+                line = bufferedReader.readLine();
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    private static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection httpURLConnection = null;
+        InputStream inputStream = null;
+        try {
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("GET");
+            httpURLConnection.setReadTimeout(10_000);
+            httpURLConnection.setConnectTimeout(15_000);
+            httpURLConnection.connect();
+
+            int responseCode = httpURLConnection.getResponseCode();
+            if (responseCode == 200) {
+                inputStream = httpURLConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(TAG, "Error response code: " + responseCode);
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Error with making http request and receiving response.", e);
+        } finally {
+            if (httpURLConnection != null) {
+                httpURLConnection.disconnect();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    private static ArrayList<Item> extractNewsFromJson(String jsonResponse) {
+        if (TextUtils.isEmpty(jsonResponse)) {
+            return null;
+        }
+
+        ArrayList<Item> items = new ArrayList<>();
+        try {
+            JSONObject rootObject = new JSONObject(jsonResponse);
+            JSONArray resultsArray = new JSONArray("results");
+
+            JSONObject jsonObject;
+            for (int i = 0; i < resultsArray.length(); i++) {
+                jsonObject = resultsArray.getJSONObject(i);
+
+                items.add(new Item(
+                        jsonObject.getString("sectionName"),
+                        jsonObject.getString("webTitle"),
+                        jsonObject.getString("webPublicationDate"),
+                        jsonObject.getString("webUrl")));
+            }
+        } catch (JSONException e) {
+            Log.e(TAG, "Error parsing JSON response.", e);
+        }
+        return items;
+    }
+}
